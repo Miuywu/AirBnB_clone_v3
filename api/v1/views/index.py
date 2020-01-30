fe87e3f41872b58a633ad1a07588a2bac31d4fb5
Miuywu <859@holbertonschool.com>
2020-01-30 07:18:44 +0000

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,6 +10,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', methods=['GET'])
 def status_ok():
     """Displays status OK"""
